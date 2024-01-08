f84af0191d4e1e5b72d08c2656b6909679975d22
mbullock710 <michael.bullock@guildeducation.com>
2024-01-07 20:40:06 -0600

    Added ability to use generateMarkdown.js file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -16,6 +17,11 @@ const questions = [
     },
     {
         type: 'input',
+        name: 'tableofcontents',
+        message: 'Please enter the table of contents for this project'
+    },
+    {
+        type: 'input',
         name: 'installation',
         message: 'Please provide the steps required to install your project.'
     },
@@ -23,24 +29,26 @@ const questions = [
         type: 'input',
         name: 'usage',
         message: 'Please provide the instructions for your project. Please also provide examples of use.'
-
-    },
-    {
-        type: 'input',
-        name: 'credits',
-        message: 'Please list any collaborators, third-party assets, or tutorials you utilized in your project.'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please provide the instructions for your project. Please also provide examples of use.'
-
     },
     {
         type: 'input',
         name: 'license',
         message: 'Please enter the license you are using.'
-
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please list guidelines for others to contribute to this project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Write tests for your application and provide examples on how to run them here.'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter your GitHub username.'
     },
 
 ]
@@ -56,11 +64,15 @@ function writeToFile(fileName, content) {
 function init() {
     inquirer.prompt(questions).then((responses) => {
         const content =
-            `# ${responses.title}
+    `# ${responses.title}
     
     ## Description
 
     ${responses.description}
+
+    ## Table of Contents
+
+    ${responses.tableofcontents}
 
     ## Installation
 
@@ -70,13 +82,21 @@ function init() {
 
     ${responses.usage}
 
-    ## Credits
-
-    ${responses.credits}
-
     ## License
     
     ${responses.license}
+
+    ## Contributing
+    
+    ${responses.contributing}
+
+    ## Tests
+    
+    ${responses.tests}
+
+    ## Questions
+    
+    ${responses.questions}
     `
 
     writeToFile('README.md', content)
